fb7160cfa17ca6841f03a883f383c86e82746c10
corepathimpact <corepathimpact@gmail.com>
2025-12-27 14:48:23 +0300

    Configure GitHub Pages deploy

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@
 // -------------------------------------------------------------
 
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
